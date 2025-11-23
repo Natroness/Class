b3b437ac012fa27a032e3dcb6f90968ac8865528
Natroness <nikeshpaudyal05@gmail.com>
2025-11-23 00:19:31 -0600

    added []

--- cardDeck.cpp
+++ cardDeck.cpp
@@ -28,10 +28,10 @@ class deck
     public:
     //also array cannot be filled directly like python so we will need a string temporary list and then add it array iteratively.
     //Also i changed this to mainDeck as we might need it back after all the cards are dealt.
-        string mainDeck[52] = {"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
-                    "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
-                    "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
-                    "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"};
+        string mainDeck[52] = {"[2H]", "[3H]", "[4H]", "[5H]", "[6H]", "[7H]", "[8H]", "[9H]", "[10H]", "[JH]", "[QH]", "[KH]", "[AH]",
+                    "[2D]", "[3D]", "[4D]", "[5D]", "[6D]", "[7D]", "[8D]", "[9D]", "[10D]", "[JD]", "[QD]", "[KD]", "[AD]",
+                    "[2C]", "[3C]", "[4C]", "[5C]", "[6C]", "[7C]", "[8C]", "[9C]", "[10C]", "[JC]", "[QC]", "[KC]", "[AC]",
+                    "[2S]", "[3S]", "[4S]", "[5S]", "[6S]", "[7S]", "[8S]", "[9S]", "[10S]", "[JS]", "[QS]", "[KS]", "[AS]"};
 
     void totalCards()
     {
@@ -111,6 +111,7 @@ int main(){
 
     for (int i = 0 ; i<3;i++)
     {
+        string card = d.dealing();
         cout<<d.dealing()<<endl;
 
     }
